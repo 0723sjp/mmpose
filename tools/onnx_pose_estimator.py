f44339749dc5bc9ccfc94957cf526a301277db33
seoilgun <seoilgun@gmail.com>
2022-06-14 16:45:58 +0900

    up

--- tools/onnx_pose_estimator.py
+++ tools/onnx_pose_estimator.py
@@ -105,6 +105,7 @@ class OnnxPoseEstimator:
         img = data['img'].unsqueeze(0)
         output_heatmap = self.sess.run(None, {self.onnx_input_key: img.detach().numpy()})
         output_heatmap = output_heatmap[0]
+        print("output_heatmap", output_heatmap.shape)
         output_heatmap = np.array([output_heatmap[0]])
         flip_output_heatmap = np.array([output_heatmap[1]])
 
