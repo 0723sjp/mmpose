660ef30f17cb264b4a79ad187d06141d1ac86b10
seoilgun <seoilgun@gmail.com>
2022-06-14 16:22:19 +0900

    up

--- tools/onnx_speed_benchmark.py
+++ tools/onnx_speed_benchmark.py
@@ -83,6 +83,7 @@ def main(args):
     onnx_results = sess.run(None, {net_feed_input[0]: onnx_input})
     print("onnx result")
     print(onnx_results)
+    print(onnx_results[0].shape)
 
     onnx_total = 0.
     for i in range(args.test_cnt):
