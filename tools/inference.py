e05f9fbbb143c0ca5a1e7bd0c3131e85ef7ccf6f
seoilgun <seoilgun@gmail.com>
2022-06-14 14:02:21 +0900

    up

--- tools/inference.py
+++ tools/inference.py
@@ -9,63 +9,37 @@ import mmcv
 import numpy as np
 from mmpose.core.bbox import bbox_xywh2xyxy, bbox_xyxy2xywh
 from mmpose.datasets.pipelines import Compose
+from mmpose.core.evaluation.top_down_eval import keypoints_from_heatmaps
 
 import onnx
 import onnxruntime as rt
 import copy
 
 
-def decode_heatmap(img_metas, output):
-    batch_size = len(img_metas)
-
-    if 'bbox_id' in img_metas[0]:
-        bbox_ids = []
-    else:
-        bbox_ids = None
-
-    c = np.zeros((batch_size, 2), dtype=np.float32)
-    s = np.zeros((batch_size, 2), dtype=np.float32)
-    image_paths = []
-    score = np.ones(batch_size)
-    for i in range(batch_size):
-        c[i, :] = img_metas[i]['center']
-        s[i, :] = img_metas[i]['scale']
-        image_paths.append(img_metas[i]['image_file'])
-
-        if 'bbox_score' in img_metas[i]:
-            score[i] = np.array(img_metas[i]['bbox_score']).reshape(-1)
-        if bbox_ids is not None:
-            bbox_ids.append(img_metas[i]['bbox_id'])
+def decode_heatmap(img_meta, output, test_cfg):
+    c = np.zeros((1, 2), dtype=np.float32)
+    s = np.zeros((1, 2), dtype=np.float32)
+    c[0, :] = img_meta['center']
+    s[0, :] = img_meta['scale']
 
     preds, maxvals = keypoints_from_heatmaps(
         output,
         c,
         s,
-        unbiased=self.test_cfg.get('unbiased_decoding', False),
-        post_process=self.test_cfg.get('post_process', 'default'),
-        kernel=self.test_cfg.get('modulate_kernel', 11),
-        valid_radius_factor=self.test_cfg.get('valid_radius_factor',
-                                              0.0546875),
-        use_udp=self.test_cfg.get('use_udp', False),
-        target_type=self.test_cfg.get('target_type', 'GaussianHeatmap'))
+        unbiased=test_cfg.get('unbiased_decoding', False),
+        post_process=test_cfg.get('post_process', 'default'),
+        kernel=test_cfg.get('modulate_kernel', 11),
+        valid_radius_factor=test_cfg.get('valid_radius_factor',
+                                         0.0546875),
+        use_udp=test_cfg.get('use_udp', False),
+        target_type=test_cfg.get('target_type', 'GaussianHeatmap'))
 
-    all_preds = np.zeros((batch_size, preds.shape[1], 3), dtype=np.float32)
-    all_boxes = np.zeros((batch_size, 6), dtype=np.float32)
+    all_preds = np.zeros((1, preds.shape[1], 3), dtype=np.float32)
     all_preds[:, :, 0:2] = preds[:, :, 0:2]
     all_preds[:, :, 2:3] = maxvals
-    all_boxes[:, 0:2] = c[:, 0:2]
-    all_boxes[:, 2:4] = s[:, 0:2]
-    all_boxes[:, 4] = np.prod(s * 200.0, axis=1)
-    all_boxes[:, 5] = score
 
-    result = {}
+    return all_preds[0]
 
-    result['preds'] = all_preds
-    result['boxes'] = all_boxes
-    result['image_paths'] = image_paths
-    result['bbox_ids'] = bbox_ids
-
-    return result
 
 def _inference_single_pose_model_onnx(sess,
                                       onnx_input_key,
@@ -117,81 +91,17 @@ def _inference_single_pose_model_onnx(sess,
     else:
         data['image_file'] = imgs_or_paths
     data = test_pipeline(data)
-    # batch_data = [data]
-    # batch_data = collate(batch_data, samples_per_gpu=len(batch_data))
-    # batch_data = scatter(batch_data, [device])[0]
 
     img_meta = data['img_metas'].data
-    print(img_meta)
-    print(data['img'].shape)
-    print(data['img'])
     img = data['img'].unsqueeze(0)
     img_flipped = img.flip(3)
     output_heatmap = sess.run(None, {onnx_input_key: img.detach().numpy()})
     output_flipped_heatmap = sess.run(None, {onnx_input_key: img_flipped.detach().numpy()})
-    print(len(output_heatmap))
-    print(output_heatmap[0].shape)
     output_heatmap = (output_heatmap[0] +
                       output_flipped_heatmap[0]) * 0.5
 
-    print(output_heatmap.shape)
+    return decode_heatmap(img_meta, output_heatmap, cfg)
 
-    decode_heatmap(img_meta, output_heatmap)
-    keypoint_result = self.keypoint_head.decode(
-        img_metas, output_heatmap, img_size=[img_width, img_height])
-
-
-#     batch_data.append(data)
-
-# #     onnx_results = sess.run(None, {onnx_input_key: onnx_input})
-
-#     def forward_test(self, img, img_metas, return_heatmap=False, **kwargs):
-#         """Defines the computation performed at every call when testing."""
-#         assert img.size(0) == len(img_metas)
-#         batch_size, _, img_height, img_width = img.shape
-#         if batch_size > 1:
-#             assert 'bbox_id' in img_metas[0]
-
-#         result = {}
-
-#         features = self.backbone(img)
-#         if self.with_neck:
-#             features = self.neck(features)
-#         if self.with_keypoint:
-#             output_heatmap = self.keypoint_head.inference_model(
-#                 features, flip_pairs=None)
-
-#         if self.test_cfg.get('flip_test', True):
-#             img_flipped = img.flip(3)
-#             features_flipped = self.backbone(img_flipped)
-#             if self.with_neck:
-#                 features_flipped = self.neck(features_flipped)
-#             if self.with_keypoint:
-#                 output_flipped_heatmap = self.keypoint_head.inference_model(
-#                     features_flipped, img_metas[0]['flip_pairs'])
-#                 output_heatmap = (output_heatmap +
-#                                   output_flipped_heatmap) * 0.5
-
-#         if self.with_keypoint:
-#             keypoint_result = self.keypoint_head.decode(
-#                 img_metas, output_heatmap, img_size=[img_width, img_height])
-#             result.update(keypoint_result)
-
-#             if not return_heatmap:
-#                 output_heatmap = None
-
-#             result['output_heatmap'] = output_heatmap
-
-#         return result
-
-#     with torch.no_grad():
-#         result = model(
-#             img=batch_data['img'],
-#             img_metas=batch_data['img_metas'],
-#             return_loss=False,
-#             return_heatmap=return_heatmap)
-
-#     return result['preds'], result['output_heatmap']
 
 def inference_top_down_pose_model_onnx(sess,
                                        onnx_input_key,
@@ -219,23 +129,17 @@ def inference_top_down_pose_model_onnx(sess,
         return [], []
 
     dataset = cfg.data.test.type
-    poses = _inference_single_pose_model_onnx(sess,
-                                              onnx_input_key,
-                                              imgs_or_paths,
-                                              bboxes_xywh[0],
-                                              dataset=dataset,
-                                              cfg=cfg)
+    pose = _inference_single_pose_model_onnx(sess,
+                                             onnx_input_key,
+                                             imgs_or_paths,
+                                             bboxes_xywh[0],
+                                             dataset=dataset,
+                                             cfg=cfg)
 
-    assert len(poses) == len(person_results), print(
-        len(poses), len(person_results), len(bboxes_xyxy))
-    pose_results = []
-    for pose, person_result, bbox_xyxy in zip(poses, person_results,
-                                              bboxes_xyxy):
-        pose_result = person_result.copy()
-        pose_result['keypoints'] = pose
-        pose_result['bbox'] = bbox_xyxy
-        pose_results.append(pose_result)
-
+    pose_result = person_results[0].copy()
+    pose_result['keypoints'] = pose
+    pose_result['bbox'] = bboxes_xyxy[0]
+    pose_results = [pose_result]
     return pose_results
 
 
@@ -384,8 +288,6 @@ def main(args):
                             print('except:', image)
 
                     start = time.time()
-                    print("bbox result")
-                    print(mmdet_results_list[index])
                     # test a single image, with a list of bboxes.
                     pose_results, returned_outputs = inference_top_down_pose_model(
                         pose_model,
@@ -402,7 +304,6 @@ def main(args):
                                                                            mmdet_results_list[index],
                                                                            bbox_thr=0.01,
                                                                            cfg=config)
-                    sys.exit()
 
                     time_pe.append(time.time() - start)
                     #                 print('-PE',image, ':', time_pe[-1])
@@ -425,8 +326,17 @@ def main(args):
                         dataset=pose_model.cfg.data.test.type,
                         show=False)
 
+                    onnx_vis_img = vis_pose_result(
+                        pose_model,
+                        img,
+                        onnx_pose_results,
+                        kpt_score_thr=0.01,
+                        dataset=pose_model.cfg.data.test.type,
+                        show=False)
+
                     index += 1
                     cv2.imwrite(args.result_dir + '/' + folder + '/' + folder2 + '/vis_' + image, vis_img)
+                    cv2.imwrite(args.result_dir + '/' + folder + '/' + folder2 + '/vis_onnx_' + image, onnx_vis_img)
 
             shutil.copy(args.result_dir + '/' + folder + '/' + folder2 + '/vis_' + folder2 + '.csv',
                         result_csv_folder + '/' + folder + '/' + folder2 + '/CollectedData_teamDLC.csv')
